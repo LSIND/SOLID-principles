1d9ecc414c974253e3ad0a0febe5544ada274068
Daria Chemkaeva <l_s_ind@hotmail.com>
2019-10-08 22:24:24 +0300

    SRP added

---
+++ SolidExamples/SolidExamplesApp/SRP/BadCar_SRP.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SolidExamplesApp.SRP
+{
+    // SRP = Single responsibility principle 
+    // bad impl
+    public class BadCarSRP
+    {
+        public int Model { get; set; }
+
+        int year;
+        public int Year
+        {
+            get { return year; }
+            set
+            {
+                if (value >= 2010 & year <= 2019)
+                    year = value;
+            }
+        }
+        public void PrintData(BadCarSRP car)
+        {
+            // Print Info about car
+        }
+    }
+}
---
+++ SolidExamples/SolidExamplesApp/SRP/GoodCar_SRP.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SolidExamplesApp.SRP
+{
+    // SRP = Single responsibility principle 
+    // good impl
+    public class GoodCarSRP
+    {
+        public int Model { get; set; }
+
+        int year;
+        public int Year
+        {
+            get { return year; }
+            set
+            {
+                if (value >= 2010 & year <= 2019)
+                    year = value;
+            }
+        }
+    }
+
+    public class Printer
+    {
+        public void PrintData(GoodCarSRP car)
+        {
+        }
+    }
+}
