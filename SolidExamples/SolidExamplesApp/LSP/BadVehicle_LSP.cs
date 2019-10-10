a285c7051b438672e5fd4779720172b097fba8e7
Daria Chemkaeva <l_s_ind@hotmail.com>
2019-10-10 11:13:19 +0300

    Create BadVehicle_LSP.cs

---
+++ SolidExamples/SolidExamplesApp/LSP/BadVehicle_LSP.cs
@@ -0,0 +1,48 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SolidExamplesApp.LSP
+{
+    public class BadVehicle
+    {
+        public int ID { get; }
+        public virtual int CalculateRent(int id)
+        {
+            return 10000;
+        }
+
+        public virtual string GetVehicleDetails(int id)
+        {
+            return "Base Vehicle";
+        }
+    }
+
+    public class Car : BadVehicle
+    {
+        public override int CalculateRent(int id)
+        {
+            return 25000;
+        }
+
+        public override string GetVehicleDetails(int id)
+        {
+            return "This is a car";
+        }
+    }
+
+    public class Bus : BadVehicle
+    {
+        // you cannot rent a Bus
+        public override int CalculateRent(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+
+        public override string GetVehicleDetails(int id)
+        {
+            return "This is a bus";
+        }
+    }
+}
