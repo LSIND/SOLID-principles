5da7658e99ae2491e7c1ab4f91ed6145281f3f5a
Daria Chemkaeva <l_s_ind@hotmail.com>
2019-10-28 22:28:27 +0300

    add LSP

---
+++ SolidExamples/SolidExamplesApp/LSP/BadVehicle_LSP.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SolidExamplesApp.LSP
+{
+    public abstract class BadVehicle_LSP
+    {
+        public virtual int CalculateRent(int id)
+        {
+            ///
+            return 10000;
+        }
+
+        public virtual string GetVehicleDetails(int id)
+        {
+            return "Base Vehicle";
+        }
+    }
+
+    public class Car : BadVehicle_LSP
+    {
+        public override int CalculateRent(int id)
+        {
+            return 25000;
+        }
+
+        public override string GetVehicleDetails(int id)
+        {
+            return "This is a car";
+        }
+    }
+
+    public class Bus : BadVehicle_LSP
+    {
+        // you cannot rent a Bus
+        public override int CalculateRent(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+
+        public override string GetVehicleDetails(int id)
+        {
+            return "This is a bus";
+
+        }
+    }
+}
---
+++ SolidExamples/SolidExamplesApp/LSP/GoodVehicle_LSP.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SolidExamplesApp.LSP
+{
+       public interface IVehicle
+        {
+            string GetVehicleDetails(int id);
+        }
+
+        public interface IPrice
+        {
+            int CalculateRent(int id);
+        }
+
+        public class GoodCar : IVehicle, IPrice
+        {
+            public int CalculateRent(int employeeId)
+            {
+                return 25000;
+            }
+
+            public string GetVehicleDetails(int employeeId)
+            {
+                return "This is a car";
+            }
+        }
+
+        public class GoodBus : IVehicle
+        {
+            public string GetVehicleDetails(int employeeId)
+            {
+                return "This is a bus";
+            }
+        }
+    }
